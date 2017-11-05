5418e103c29a20f29476c8b2b703182cd5efcfbc
Sander Verdonschot <5864726+Mangara@users.noreply.github.com>
2017-11-04 20:11:54 -0400

    Initial commit

---
+++ src/codecup2018/RandomPlayer.java
@@ -0,0 +1,54 @@
+package codecup2018;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class RandomPlayer extends Player {
+
+    private static final Random rand = new Random();
+
+    public RandomPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    protected byte[] selectMove() {
+        // Pick a random free square
+        int nOpen = 0;
+
+        for (byte a = 0; a < 8; a++) {
+            for (byte b = 0; b < 8 - a; b++) {
+                if (board.get(a, b) == Board.FREE) {
+                    nOpen++;
+                }
+            }
+        }
+
+        int pick = rand.nextInt(nOpen);
+        byte moveA = -1, moveB = -1;
+
+        for (byte a = 0; a < 8; a++) {
+            for (byte b = 0; b < 8 - a; b++) {
+                if (board.get(a, b) == Board.FREE) {
+                    if (pick == 0) {
+                        moveA = a;
+                        moveB = b;
+                    }
+                    pick--;
+                }
+            }
+        }
+
+        // Pick a random free number
+        byte moveVal = (byte) (int) myNumbers.get(rand.nextInt(myNumbers.size()));
+
+        return new byte[]{moveA, moveB, moveVal};
+    }
+    
+    public static void main(String[] args) throws IOException {
+        RandomPlayer r = new RandomPlayer("Random");
+        r.play(new BufferedReader(new InputStreamReader(System.in)), System.out);
+    }
+}
---
+++ src/codecup2018/Tournament.java
@@ -0,0 +1,89 @@
+package codecup2018;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Tournament {
+
+    private static final int GAMES = 1000;
+
+    public static void main(String[] args) {
+        runTournament(Arrays.asList(new RandomPlayer("Rando"), new ExpectedPlayer("Expy")));
+    }
+
+    public static void runTournament(List<Player> players) {
+        int n = players.size();
+        int[][] wins = new int[n][n];
+        double[][] avgScore = new double[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                double totalScore = 0;
+
+                for (int k = 0; k < GAMES; k++) {
+                    int score = GameHost.runGame(players.get(i), players.get(j), false);
+                    totalScore += score;
+
+                    if (score > 0) {
+                        wins[i][j]++;
+                    } else if (score < 0) {
+                        wins[j][i]++;
+                    }
+                }
+
+                avgScore[i][j] = totalScore / GAMES;
+                avgScore[j][i] = -avgScore[i][j];
+            }
+        }
+
+        report(players, wins, avgScore);
+    }
+
+    private static void report(List<Player> players, int[][] wins, double[][] avgScore) {
+        // List players
+        System.out.println("Players:");
+        for (int i = 0; i < players.size(); i++) {
+            System.out.printf("%d: %s%n", i, players.get(i).getName());
+        }
+        System.out.println();
+
+        // Print wins
+        int WIN_CELL_WIDTH = 5;
+        
+        System.out.println("Number of wins:");
+
+        System.out.printf("%" + WIN_CELL_WIDTH + "s", "");
+        for (int i = 0; i < players.size(); i++) {
+            System.out.printf("%" + WIN_CELL_WIDTH + "d", i);
+        }
+        System.out.println();
+
+        for (int i = 0; i < players.size(); i++) {
+            System.out.printf("%" + WIN_CELL_WIDTH + "d", i);
+            for (int j = 0; j < players.size(); j++) {
+                System.out.printf("%" + WIN_CELL_WIDTH + "d", wins[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        
+        // Print averages
+        int AVG_CELL_WIDTH = 6;
+        
+        System.out.println("Average score:");
+        
+        System.out.printf("%" + AVG_CELL_WIDTH + "s", "");
+        for (int i = 0; i < players.size(); i++) {
+            System.out.printf("%" + AVG_CELL_WIDTH + "d", i);
+        }
+        System.out.println();
+
+        for (int i = 0; i < players.size(); i++) {
+            System.out.printf("%" + AVG_CELL_WIDTH + "d", i);
+            for (int j = 0; j < players.size(); j++) {
+                System.out.printf("%" + AVG_CELL_WIDTH + "." + (AVG_CELL_WIDTH - 3) + "g", avgScore[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
