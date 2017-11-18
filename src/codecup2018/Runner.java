a8ed4503d734eebfbac277f85dfef245ab2a31c0
Sander Verdonschot <5864726+Mangara@users.noreply.github.com>
2017-11-18 10:15:49 -0500

    Misc changes

--- src/codecup2018/Runner.java
+++ src/codecup2018/Runner.java
@@ -1,7 +1,7 @@
 package codecup2018;
 
 import codecup2018.evaluator.ExpectedValue;
-import codecup2018.movegenerator.AllMoves;
+import codecup2018.movegenerator.NoHoles;
 import codecup2018.player.SimpleMaxPlayer;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +10,6 @@ import java.io.InputStreamReader;
 public class Runner {
 
     public static void main(String[] args) throws IOException {
-        new SimpleMaxPlayer("Expy", new ExpectedValue(), new AllMoves()).play(new BufferedReader(new InputStreamReader(System.in)), System.out);
+        new SimpleMaxPlayer("Expy_NH", new ExpectedValue(), new NoHoles()).play(new BufferedReader(new InputStreamReader(System.in)), System.out);
     }
 }
