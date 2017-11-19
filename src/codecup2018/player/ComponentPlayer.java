fded36c9361b81c63671a5110a29897a425d1aa2
Mangara <sander.verdonschot@gmail.com>
2017-11-19 18:07:37 -0500

    Progress on ComponentPlayer

--- src/codecup2018/player/ComponentPlayer.java
+++ src/codecup2018/player/ComponentPlayer.java
@@ -1,11 +1,15 @@
 package codecup2018.player;
 
 import codecup2018.Board;
+import codecup2018.Pair;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class ComponentPlayer extends Player {
 
-    protected List<Board> components;
+    private static final byte VISITED = 119;
+    protected List<Pair<Board, Integer>> components;
 
     public ComponentPlayer(String name) {
         super(name);
@@ -14,7 +18,41 @@ public abstract class ComponentPlayer extends Player {
     @Override
     public void initialize(Board currentBoard) {
         super.initialize(currentBoard);
-        // TODO: set up components
+        
+        Board tempBoard = new Board(currentBoard);
+
+        for (byte a = 0; a < 8; a++) {
+            for (byte b = 0; b < 8 - a; b++) {
+                if (tempBoard.get(a, b) == Board.FREE) {
+                    // Mark each cell of this connected component as visited
+                    markComponent(tempBoard, a, b);
+                    
+                    Board component = new Board(tempBoard);
+                    int size = 0;
+                    
+                    for (byte a2 = 0; a2 < 8; a2++) {
+                        for (byte b2 = 0; b2 < 8 - a2; b2++) {
+                            if (tempBoard.get(a2, b2) == Board.FREE) {
+                                component.set(a2, b2, Board.BLOCKED);
+                            } else if (tempBoard.get(a2, b2) == VISITED) {
+                                component.set(a2, b2, Board.FREE);
+                                tempBoard.set(a2, b2, Board.BLOCKED);
+                                size++;
+                            }
+                        }
+                    }
+                    
+                    components.add(new Pair<>(component, size));
+                }
+            }
+        }
+
+        Collections.sort(components, new Comparator<Pair<Board, Integer>>() {
+            @Override
+            public int compare(Pair<Board, Integer> o1, Pair<Board, Integer> o2) {
+                return -Integer.compare(o1.getSecond(), o2.getSecond());
+            }
+        });
     }
 
     @Override
@@ -27,5 +65,9 @@ public abstract class ComponentPlayer extends Player {
     public void processMove(byte[] move, boolean mine) {
         super.processMove(move, mine);
         // TODO: update components
+    }
+
+    private void markComponent(Board board, byte a, byte b) {
+        // DFS
     }
 }
