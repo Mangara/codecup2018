85231b64d591ec3c278afc7c549ed1e989c20b93
Sander Verdonschot <sander.verdonschot@gmail.com>
2017-11-22 08:23:46 -0500

    Finished ComponentPlayer

--- src/codecup2018/player/ComponentPlayer.java
+++ src/codecup2018/player/ComponentPlayer.java
@@ -2,14 +2,23 @@ package codecup2018.player;
 
 import codecup2018.Board;
 import codecup2018.Pair;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.ListIterator;
 
 public abstract class ComponentPlayer extends Player {
 
     private static final byte VISITED = 119;
-    protected List<Pair<Board, Integer>> components;
+    private static final Comparator<Board> COMPONENT_SIZE = new Comparator<Board>() {
+        @Override
+        public int compare(Board o1, Board o2) {
+            return -Integer.compare(o1.getFreeSpots(), o2.getFreeSpots());
+        }
+    };
+
+    protected final List<Board> components = new ArrayList<>();
 
     public ComponentPlayer(String name) {
         super(name);
@@ -19,55 +28,203 @@ public abstract class ComponentPlayer extends Player {
     public void initialize(Board currentBoard) {
         super.initialize(currentBoard);
         
+        components.clear();
         Board tempBoard = new Board(currentBoard);
 
         for (byte a = 0; a < 8; a++) {
             for (byte b = 0; b < 8 - a; b++) {
                 if (tempBoard.get(a, b) == Board.FREE) {
-                    // Mark each cell of this connected component as visited
                     markComponent(tempBoard, a, b);
-                    
-                    Board component = new Board(tempBoard);
-                    int size = 0;
-                    
-                    for (byte a2 = 0; a2 < 8; a2++) {
-                        for (byte b2 = 0; b2 < 8 - a2; b2++) {
-                            if (tempBoard.get(a2, b2) == Board.FREE) {
-                                component.set(a2, b2, Board.BLOCKED);
-                            } else if (tempBoard.get(a2, b2) == VISITED) {
-                                component.set(a2, b2, Board.FREE);
-                                tempBoard.set(a2, b2, Board.BLOCKED);
-                                size++;
-                            }
-                        }
-                    }
-                    
-                    components.add(new Pair<>(component, size));
+
+                    /*/// DEBUG
+                    System.err.println("After marking component");
+                    System.err.println("Main board:");
+                    tempBoard.print();
+                    System.err.println();
+                    ///*/
+                    Board component = extractComponentBoard(tempBoard);
+
+                    /*/// DEBUG
+                    System.err.println("After extracting component");
+                    System.err.println("Main board:");
+                    tempBoard.print();
+                    System.err.println("Component:");
+                    component.print();
+                    System.err.println();
+                    ///*/
+                    components.add(component);
                 }
             }
         }
 
-        Collections.sort(components, new Comparator<Pair<Board, Integer>>() {
-            @Override
-            public int compare(Pair<Board, Integer> o1, Pair<Board, Integer> o2) {
-                return -Integer.compare(o1.getSecond(), o2.getSecond());
-            }
-        });
+        Collections.sort(components, COMPONENT_SIZE);
+
+        /*/// DEBUG
+        System.err.println("Components:");
+        for (Pair<Board, Integer> component : components) {
+            System.err.println("Component of size " + component.getSecond());
+            component.getFirst().print();
+            System.err.println();
+        }
+        ///*/
     }
 
     @Override
-    public void block(String loc) {
-        super.block(loc);
-        // TODO: update components
+    public void block(byte a, byte b) {
+        super.block(a, b);
+        updateComponentsForMove(a, b, Board.BLOCKED);
     }
 
     @Override
     public void processMove(byte[] move, boolean mine) {
+        /*/// DEBUG
+        System.err.printf("Before processing move (%d, %d) = %d:%n", move[0], move[1], move[2]);
+        board.print();
+        System.err.println("Components:");
+        for (Pair<Board, Integer> c : components) {
+            System.err.println("Component of size " + c.getSecond());
+            c.getFirst().print();
+        }
+        System.err.println();
+        ///*/
+        
         super.processMove(move, mine);
-        // TODO: update components
+        updateComponentsForMove(move[0], move[1], (mine ? move[2] : (byte) -move[2]));
+        
+        /*/// DEBUG
+        System.err.printf("After processing move (%d, %d) = %d:%n", move[0], move[1], move[2]);
+        board.print();
+        System.err.println("Components:");
+        int totalFree = 0;
+        for (Pair<Board, Integer> c : components) {
+            System.err.println("Component of size " + c.getSecond());
+            c.getFirst().print();
+            totalFree += c.getFirst().getFreeSpots();
+        }
+        System.err.println();
+        
+        if (board.getFreeSpots() != totalFree) {
+            throw new InternalError();
+        }
+        ///*/
+    }
+
+    private void updateComponentsForMove(byte a, byte b, byte value) {
+        // Set this value everywhere and find the component that contains this move
+        int playedComponent = -1;
+
+        for (int i = 0; i < components.size(); i++) {
+            Board bb = components.get(i);
+
+            if (bb.get(a, b) == Board.FREE) {
+                playedComponent = i;
+            }
+
+            bb.set(a, b, value);
+        }
+
+        // Find the new components after this move
+        Board component = components.remove(playedComponent);
+        List<Board> newComponents = split(component, a, b);
+
+        for (Board c : newComponents) {
+            int i = Collections.binarySearch(components, c, COMPONENT_SIZE);
+
+            if (i < 0) {
+                i = -(i + 1);
+            }
+
+            components.add(i, c);
+        }
+
+        /*/// DEBUG
+        System.err.printf("Components after setting (%d, %d):%n", a, b);
+        for (Pair<Board, Integer> c : components) {
+            System.err.println("Component of size " + c.getSecond());
+            c.getFirst().print();
+            System.err.println();
+        }
+        ///*/
     }
 
     private void markComponent(Board board, byte a, byte b) {
-        // DFS
+        board.set(a, b, VISITED);
+
+        if (a > 0 && board.get((byte) (a - 1), b) == Board.FREE) {
+            markComponent(board, (byte) (a - 1), b);
+        }
+
+        if (a < 7 - b && board.get((byte) (a + 1), b) == Board.FREE) {
+            markComponent(board, (byte) (a + 1), b);
+        }
+
+        if (b > 0 && board.get(a, (byte) (b - 1)) == Board.FREE) {
+            markComponent(board, a, (byte) (b - 1));
+        }
+
+        if (b < 7 - a && board.get(a, (byte) (b + 1)) == Board.FREE) {
+            markComponent(board, a, (byte) (b + 1));
+        }
+
+        if (a > 0 && b < 8 - a && board.get((byte) (a - 1), (byte) (b + 1)) == Board.FREE) {
+            markComponent(board, (byte) (a - 1), (byte) (b + 1));
+        }
+
+        if (a < 8 - b && b > 0 && board.get((byte) (a + 1), (byte) (b - 1)) == Board.FREE) {
+            markComponent(board, (byte) (a + 1), (byte) (b - 1));
+        }
+    }
+
+    private Board extractComponentBoard(Board fullBoard) {
+        Board componentBoard = new Board(fullBoard);
+
+        for (byte a = 0; a < 8; a++) {
+            for (byte b = 0; b < 8 - a; b++) {
+                if (fullBoard.get(a, b) == Board.FREE) {
+                    componentBoard.set(a, b, Board.BLOCKED);
+                } else if (fullBoard.get(a, b) == VISITED) {
+                    componentBoard.set(a, b, Board.FREE);
+                    fullBoard.set(a, b, Board.BLOCKED);
+                }
+            }
+        }
+
+        return componentBoard;
+    }
+
+    private List<Board> split(Board cBoard, byte a, byte b) {
+        List<Board> newComponents = new ArrayList<>();
+
+        if (a > 0 && cBoard.get((byte) (a - 1), b) == Board.FREE) {
+            markComponent(cBoard, (byte) (a - 1), b);
+            newComponents.add(extractComponentBoard(cBoard));
+        }
+
+        if (a < 7 - b && cBoard.get((byte) (a + 1), b) == Board.FREE) {
+            markComponent(cBoard, (byte) (a + 1), b);
+            newComponents.add(extractComponentBoard(cBoard));
+        }
+
+        if (b > 0 && cBoard.get(a, (byte) (b - 1)) == Board.FREE) {
+            markComponent(cBoard, a, (byte) (b - 1));
+            newComponents.add(extractComponentBoard(cBoard));
+        }
+
+        if (b < 7 - a && cBoard.get(a, (byte) (b + 1)) == Board.FREE) {
+            markComponent(cBoard, a, (byte) (b + 1));
+            newComponents.add(extractComponentBoard(cBoard));
+        }
+
+        if (a > 0 && b < 8 - a && cBoard.get((byte) (a - 1), (byte) (b + 1)) == Board.FREE) {
+            markComponent(cBoard, (byte) (a - 1), (byte) (b + 1));
+            newComponents.add(extractComponentBoard(cBoard));
+        }
+
+        if (a < 8 - b && b > 0 && cBoard.get((byte) (a + 1), (byte) (b - 1)) == Board.FREE) {
+            markComponent(cBoard, (byte) (a + 1), (byte) (b - 1));
+            newComponents.add(extractComponentBoard(cBoard));
+        }
+
+        return newComponents;
     }
 }
