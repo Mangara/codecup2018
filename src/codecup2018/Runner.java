24714015318873ffc7a7000ab958613e8287612c
Sander Verdonschot <5864726+Mangara@users.noreply.github.com>
2017-12-04 12:19:17 -0500

    Small bugfixes.

--- src/codecup2018/Runner.java
+++ src/codecup2018/Runner.java
@@ -2,7 +2,7 @@ package codecup2018;
 
 import codecup2018.evaluator.IncrementalExpectedValue;
 import codecup2018.movegenerator.MaxInfluenceMoves;
-import codecup2018.player.AspirationPlayer;
+import codecup2018.player.MultiAspirationTablePlayer;
 import codecup2018.player.Player;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,6 +20,6 @@ public class Runner {
     public static Player getPlayer() {
         //return new SimpleMaxPlayer("Expy", new ExpectedValue(), new AllMoves()); // Played Nov 11 Test Competition
         //return new AspirationPlayer("As_MF_MFM_10", new MedianFree(), new MostFreeMax(), 10); // Played Nov 25 Test Competition (did worse and timed out 8 times)
-        return new AspirationPlayer("As_IEV_MI_6", new IncrementalExpectedValue(), new MaxInfluenceMoves(), 6);
+        return new MultiAspirationTablePlayer("MAsT_IEV_MI_6", new IncrementalExpectedValue(), new MaxInfluenceMoves(), 6);
     }
 }
